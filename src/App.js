84d565eef06eb0d027074fb5cc03a1feef376163
takwaibrahimmohamed <takwaibrahimmohamed2002@gmail.com>
2023-09-18 02:46:13 -0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './component/Footer';
+
+import Header from './component/Header';
+import Text from './component/Text';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+{/*      
+      <Text/> */}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
